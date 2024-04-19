ed06959d0a4acd73f4e2344fd6af166b78756693
Batas <nischal.batas88@gmail.com>
2024-04-19 21:25:54 +0545

    creating generated ai page

--- src/components/Page/NavMenu/Services/AI/GenerativeAI/content/Fields.js
+++ src/components/Page/NavMenu/Services/AI/GenerativeAI/content/Fields.js
@@ -1,20 +1,20 @@
 import Image from "next/image";
 import React from "react";
+import './business.css'
 import { empowerIndustries } from "@/db/empowerIndustries";
 const Fields = () => {
   return (
-    <div className="flex flex-wrap justify-center md:justify-start gap-4 md:gap-8 mt-4">
+    <div className="empower-main flex flex-wrap justify-center md:justify-start gap-4 md:gap-8 mt-4">
       {empowerIndustries.map((item, index) => {
         return (
           <div key={index}
-            className="relative field-content bg-no-repeat rounded border border-black"
+            className="empower-card relative field-content bg-no-repeat rounded border border-black"
             style={{
-              width: "295px",
-              height: "396px",
+            
               backgroundImage: `url(${item.image})`,
             }}
           >
-            <div className="flex flex-col justify-between h-full ">
+            <div className="empower-title flex flex-col justify-between h-full ">
               <div className="flex justify-end pt-2 pr-2 opacity-55">
                 <button className="bg-cloud2 px-3 py-1 text-xs font-medium capitalize rounded">
                   {item.sector}

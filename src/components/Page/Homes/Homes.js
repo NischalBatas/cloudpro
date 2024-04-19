4e84787e4e5ef905d1528b1719aa1ad5a0a1e79c
Batas <nischal.batas88@gmail.com>
2024-04-20 00:20:55 +0545

    creating generated ai page

--- src/components/Page/Homes/Homes.js
+++ src/components/Page/Homes/Homes.js
@@ -13,6 +13,18 @@ const Homes = () => {
       <Service/>
       <Features/>
       <Industries/>
+      {/* <div className="my-10 relative m-auto w-96">
+        <div className="bg-cloud line-svg" style={{width:'20px', height:'20px'}}></div>
+        <svg width="194" height="93">
+        <path d="M1 0.5V45.5C1 54.3366 8.16344 61.5 17 61.5H177C185.837 61.5 193 68.6634 193 77.5V93" stroke="#242424" fill="none"/>
+
+        </svg>
+      </div>
+
+      <div className="my-10 relative m-auto w-96">
+        <div className="bg-cloud lines-svg" style={{width:'100px', height:'20px'}}></div>
+     
+      </div> */}
     </div>
   );
 };
--- static/js/script.js
+++ static/js/script.js
@@ -1,47 +1,41 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  new WOW().init();
 
-    new WOW().init();
+  $(".team .owl-carousel").owlCarousel({
+    loop: true,
+    margin: 5,
+    autoplay: false,
+    dots: false,
+    nav: false,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      324: {
+        items: 4,
+      },
+      680: {
+        items: 6,
+      },
+      1100: {
+        items: 7,
+      },
+    },
+  });
+});
 
-
-    $(".team .owl-carousel").owlCarousel({
-        loop: true,
-        margin: 5,
-        autoplay: false,
-        dots: false,
-        nav: false,
-        responsiveClass: true,
-        responsive: {
-          0: {
-            items: 1,
-          },
-          324: {
-            items: 4,
-          },
-          680: {
-            items: 6,
-          },
-          1100: {
-            items: 7,
-          }
-        },
-      });
-
-    })
-
-
-
-    let valueDisplays = document.querySelectorAll(".num");
-    let interval = 1;
-    valueDisplays.forEach((valueDisplay) => {
-      let startValue = 0;
-      let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-      let duration = Math.floor(interval / endValue);
-      let counter = setInterval(function () {
-        startValue += 1 ;
-        valueDisplay.textContent = startValue;
-        if (startValue == endValue) {
-          clearInterval(counter);
-        }
-      }, duration);
-    });
-    
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 1;
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, 150);
+});

a4fafe8046465c7a7a23e105bde55f1ca58b6641
Batas <nischal.batas88@gmail.com>
2024-04-29 00:03:19 +0545

    removing navbar-main

--- src/components/Page/Homes/Content/ServiceItem.js
+++ src/components/Page/Homes/Content/ServiceItem.js
@@ -7,7 +7,7 @@ const ServiceItem = () => {
     <div className="grid grid-cols-2 md:grid-cols-3 mt-10 gap-4 gap-y-5">
       {serviceItem.map((item,index) => {
         return (
-          <div key={index} className="md:max-w-[330px] bg-[#101218] p-4 rounded-md border border-[#1E2849] flex flex-col items-center">
+          <div key={index} className="md:max-w-[330px] bg-[#0e0e1a] p-4 rounded-md border border-[#1E2849] flex flex-col items-center">
             <div className="mb-5">
               <p className="text-white mb-1 text-[16px] font-semibold max-w-[265px] text-left">
                 {item.title}

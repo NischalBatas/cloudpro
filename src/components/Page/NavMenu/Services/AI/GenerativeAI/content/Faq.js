d963c38b36e09453bf13c5bfa932efd93890464a
Batas <nischal.batas88@gmail.com>
2024-04-29 17:03:56 +0545

    generative page

--- src/components/Page/NavMenu/Services/AI/GenerativeAI/content/Faq.js
+++ src/components/Page/NavMenu/Services/AI/GenerativeAI/content/Faq.js
@@ -5,44 +5,20 @@ import "./faq.css";
 
 const Faq = () => {
   return (
-    <div>
+    <div className="navbar-main py-20">
       <div>
-        <p>FAQ</p>
-        <p>Generative AI & Applications</p>
+        <p className="font-bold text-[36px]">FAQ</p>
+        <p className="text-[16px] text=[#BCBCBC]">Generative AI & Applications</p>
       </div>
 
-      <div>
-        <Accordion.Root
-          className="AccordionRoot"
-          type="single"
-          defaultValue="item-1"
-          collapsible
-        >
-          <Accordion.Item className="AccordionItem" value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
-            </AccordionContent>
-          </Accordion.Item>
-
-          <Accordion.Item className="AccordionItem" value="item-2">
-            <AccordionTrigger>Is it unstyled?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It's unstyled by default, giving you freedom over the look
-              and feel.
-            </AccordionContent>
-          </Accordion.Item>
-
-          <Accordion.Item className="AccordionItem" value="item-3">
-            <AccordionTrigger>Can it be animated?</AccordionTrigger>
-            <Accordion.Content className="AccordionContent">
-              <div className="AccordionContentText">
-                Yes! You can animate the Accordion with CSS or JavaScript.
-              </div>
-            </Accordion.Content>
-          </Accordion.Item>
-        </Accordion.Root>
-      </div>
+      <Accordion.Root>
+    <Accordion.Item>
+      <Accordion.Header>
+        <Accordion.Trigger />
+      </Accordion.Header>
+      <Accordion.Content />
+    </Accordion.Item>
+  </Accordion.Root>
     </div>
   );
 };

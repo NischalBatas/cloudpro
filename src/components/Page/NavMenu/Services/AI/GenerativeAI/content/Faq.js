bcb5aeceed1212b627113660acae7de0cae13a3b
Batas <nischal.batas88@gmail.com>
2024-04-29 18:48:03 +0545

    generative page

--- src/components/Page/NavMenu/Services/AI/GenerativeAI/content/Faq.js
+++ src/components/Page/NavMenu/Services/AI/GenerativeAI/content/Faq.js
@@ -1,24 +1,39 @@
+"use client"
 import React from "react";
 import * as Accordion from '@radix-ui/react-accordion';
-
+import { FaAngleDown } from "react-icons/fa6";
 import "./faq.css";
+import { IoChevronDownCircleOutline } from "react-icons/io5";
+import { faqApp } from "@/db/faq";
 
 const Faq = () => {
   return (
-    <div className="navbar-main py-20">
-      <div>
-        <p className="font-bold text-[36px]">FAQ</p>
-        <p className="text-[16px] text=[#BCBCBC]">Generative AI & Applications</p>
+    <div className="navbar-main py-20 flex justify-between gap-2">
+      <div className="w-[219px]">
+        <p className="font-bold text-[36px] w-[219px]">FAQ</p>
+        <p className="text-[16px] text=[#BCBCBC] w-[219px]">Generative AI & Applications</p>
       </div>
 
-      <Accordion.Root>
-    <Accordion.Item>
-      <Accordion.Header>
-        <Accordion.Trigger />
-      </Accordion.Header>
-      <Accordion.Content />
+      <Accordion.Root className="AccordionRoot" type="single" defaultValue="item-1" collapsible>
+  {faqApp.map((item,index)=>{
+    return(
+      <Accordion.Item key={index} className="AccordionItem" value={`item-${item.number}`}>
+      <Accordion.Trigger className="AccordionTrigger text-[14px]">{item.question}<FaAngleDown className="ml-2 FaAngleDown"/> </Accordion.Trigger>
+      <div className="line-faq"></div >
+      <Accordion.Content className="AccordionContent"> 
+      <div className="AccordionContentText">
+         {item.answer}
+        </div>
+        </Accordion.Content>
+  
     </Accordion.Item>
-  </Accordion.Root>
+    )
+  })}
+ 
+
+  
+</Accordion.Root>
+
     </div>
   );
 };
---
+++ src/db/faq.js
@@ -0,0 +1,11 @@
+export const faqApp=[
+    {number:1,question:"What is Generative AI, and how does it differ from traditional AI", answer:"To use a negative letter-spacing value, prefix the class name with a dash to convert it to a negative value."},
+    {number:2,question:"What is Generative AI, and how does it differ from traditional AI", answer:"To use a negative letter-spacing value, prefix the class name with a dash to convert it to a negative value."},
+    {number:3,question:"What is Generative AI, wwwwwwwwwwwwww www and how does it differ from traditional AI", answer:"To use a negative letter-spacing value, prefix the class name with a dash to convert it to a negative value."},
+    {number:4,question:"What is Generative AI, and how does it differ from traditional AI", answer:"To use a negative letter-spacing value, prefix the class name with a dash to convert it to a negative value."},
+    {number:5,question:"What is Generative AI, and how does it differ from traditional AI", answer:"To use a negative letter-spacing value, prefix the class name with a dash to convert it to a negative value."},
+    {number:6,question:"What is Generative AI, and how does it differ from traditional AI", answer:"To use a negative letter-spacing value, prefix the class name with a dash to convert it to a negative value."},
+    {number:7,question:"What is Generative AI, and how does it differ from traditional AI", answer:"To use a negative letter-spacing value, prefix the class name with a dash to convert it to a negative value."},
+    {number:8,question:"What is Generative AI, and how does it differ from traditional AI", answer:"To use a negative letter-spacing value, prefix the class name with a dash to convert it to a negative value."},
+
+]

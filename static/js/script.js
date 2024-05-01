db473246c2972a8ed5ddd8424912774cb69dffc0
Batas <nischal.batas88@gmail.com>
2024-05-01 09:54:08 +0545

    logo updated

--- static/js/script.js
+++ static/js/script.js
@@ -90,7 +90,7 @@ valueDisplays.forEach((valueDisplay) => {
 $(document).ready(function () {
   $(window).scroll(function () {
     let pos = $(window).scrollTop();
-    if (pos >= 50) {
+    if (pos >= 28) {
       $(".navbar-main-group").addClass("navbar-main-group2");
       $(".blacklogo").addClass("blacklogo2");
       $(".whitelogo").addClass("whitelogo2");

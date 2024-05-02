4adf7789b5495c200d6f4e1b0e012e5127d2999b
Batas <nischal.batas88@gmail.com>
2024-05-02 11:17:44 +0545

    adding animation

--- src/components/Page/Homes/Content/Introduction.js
+++ src/components/Page/Homes/Content/Introduction.js
@@ -1,10 +1,110 @@
+"use client"
 import Image from "next/image";
 import React from "react";
 import { MdOutlineArrowOutward } from "react-icons/md";
+
+import { useCallback, useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from '@tsparticles/react';
+import { loadSlim } from '@tsparticles/slim';
+
+
+
+
 const Introduction = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    const initializeParticles = async () => {
+      await initParticlesEngine(async (engine) => {
+        await loadSlim(engine);
+      }).then(() => {
+        setInit(true);
+      });
+    };
+
+    initializeParticles();
+  }, []);
+
+  const particlesLoaded = useCallback((container) => {
+    console.log(container);
+  }, []);
+
   return (
+    <>
+  
 
       <div className="main-container pb-40 relative">
+      { init && <Particles
+      id="tsparticles"
+      particlesLoaded={particlesLoaded}
+      options={{
+          background: {
+              
+          },
+          fpsLimit: 120,
+          interactivity: {
+              events: {
+                  onClick: {
+                      enable: true,
+                      mode: "push",
+                  },
+                  onHover: {
+                      enable: true,
+                      mode: "repulse",
+                  },
+                  resize: true,
+              },
+              modes: {
+                  push: {
+                      quantity: 4,
+                  },
+                  repulse: {
+                      distance: 200,
+                      duration: 0.4,
+                  },
+              },
+          },
+          particles: {
+              color: {
+                  value: "#ffffff",
+              },
+              links: {
+                  color: "#ffffff",
+                  distance: 100,
+                  enable: true,
+                  opacity: 0.5,
+                  width: 1,
+              },
+              move: {
+                  direction: "none",
+                  enable: true,
+                  outModes: {
+                      default: "bounce",
+                  },
+                  random: false,
+                  speed: 6,
+                  straight: false,
+              },
+              number: {
+                  density: {
+                      enable: true,
+                      area: 800,
+                  },
+                  value: 280,
+              },
+              opacity: {
+                  value: 0.5,
+              },
+              shape: {
+                  type: "circle",
+              },
+              size: {
+                  value: { min: 1, max: 4 },
+              },
+          },
+          detectRetina: true,
+      }}
+  />}
         <div className="intro-image w-full h-[742px] absolute bg-no-repeat bg-center"  style={{backgroundImage:"url('/Image/background.svg')",zIndex:'-1'}}>
         </div>
         <div className="pt-44 mt- z-20">
@@ -31,7 +131,7 @@ const Introduction = () => {
           </div>
         </div>
       </div>
-  
+  </>
   );
 };
 

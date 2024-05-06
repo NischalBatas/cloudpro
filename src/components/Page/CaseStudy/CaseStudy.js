08b25b5bc9b5a808c723a5badb40297545680582
Batas <nischal.batas88@gmail.com>
2024-05-06 22:24:37 +0545

    adding case study pages

--- src/components/Page/CaseStudy/CaseStudy.js
+++ src/components/Page/CaseStudy/CaseStudy.js
@@ -1,15 +1,64 @@
+import { caseStudy } from "@/db/casestudy/casestudy";
+import Link from "next/link";
 import React from "react";
-
+import { MdOutlineArrowOutward } from "react-icons/md";
+import './casestudy.css'
 const CaseStudy = () => {
   return (
-    <div>
-      <div className="h-[550px]  mt-12 text-center  flex flex-col justify-center items-center">
+    <div className="">
+      <div className="h-[553px]  mt-12 text-center  flex flex-col justify-center items-center">
         <p className="text-[48px] font-bold">Case Studies</p>
         <p className="text-[14px] leading-6 max-w-[514px]">
           We're Gen AI-Ready and Eager to Collaborate. Let's Assess Your
           Business and Infrastructure Readiness for the Ultimate Technological
           Leap.
         </p>
+      </div>
+
+      <div className="navbar-main bg-white ">
+        <div className="main-container relative bottom-28  grid md:grid-cols-2 lg:grid-cols-3 gap-3 my-6">
+          {caseStudy.map((item, index) => {
+            return (
+              <Link href='/casestudy' className="">
+                <div key={index} className="md:w-80 casestudy_product_container">
+                  <div
+                    className="h-52 md:w-80 relative  bg-no-repeat bg-cover rounded-lg"
+                    style={{
+                      backgroundImage: `url(${item.image})`,
+                    }}
+                  >
+                    <div className="flex justify-between  px-4 pt-4  ">
+                      <div className="text-white text-base  font-medium w-44">
+                        {item.sector}
+                      </div>
+                      <div className="casestudy_product_arrow">
+                      <MdOutlineArrowOutward className=" bg-white rounded-full text-black w-6 h-6 text-xs p-1" />
+                    </div>
+                    </div>
+                  </div>
+
+                  <div className="flex gap-2 items-center text-xs mt-3 text-white">
+                    <div className="bg-cloud px-2 rounded-full py-1">
+                      {item.category}
+                    </div>
+                    <div className="text-[10px] text-[#909090] tracking-wider">
+                      {item.published}
+                    </div>
+                  </div>
+
+                  <div className="my-4">
+                    <div className="casestudy_product_title font-semibold text-[16px] text-[#000]">
+                      {item.title}
+                    </div>
+                    <div className="casestudy_product_description mt-2 text-[14px] text-[#666666] leading-6">
+                      {item.description}
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/db/casestudy/casestudy.js
+++ src/db/casestudy/casestudy.js
@@ -5,8 +5,8 @@ export const caseStudy = [
       published: "2 Min Read",
       title: "Transforming Healthcare Operations with Amazon Web Services",
       description:
-        "We're Gen AI-Ready and Eager to Collaborate. Let's Assess Your Business and Infrastructure Readiness for the Ultimate Technological Leap.",
-      image: "/Image/fields/Maskgroup-4.jpg",
+        "We're Gen AI-Ready and Eager to Collaborate. Let's Assess Your Business and Infrastructure Readiness for the Ultimate Technological Leap.Readiness for the Ultimate Technological Leap.",
+      image: "/Image/casestudy/1.jpg",
     },
     {
       sector: "AI Chatbots",
@@ -14,8 +14,8 @@ export const caseStudy = [
       published: "2 Min Read",
       title: "Revolutionizing Customer Service with AWS-Powered AI Chatbots for a Leading Retail Chain",
       description:
-        "In collaboration with Cloud Pro AI, the Company embarked on a journey to overhaul its custom.",
-      image: "/Image/fields/Maskgroup-3.jpg",
+        "In collaboration with Cloud Pro AI, the Company embarked on a journey to overhaul its custom. Readiness for the Ultimate Technological Leap.Readiness for the Ultimate Technological Leap.",
+      image: "/Image/casestudy/2.jpg",
     },
     {
       sector: "Billing and Compliance with Cloud Pro AI",
@@ -24,7 +24,64 @@ export const caseStudy = [
       title: "Transforming Healthcare Billing and Compliance with Cloud Pro AI and Amazon Web Services",
       description:
         "We're Gen AI-Ready and Eager to Collaborate. Let's Assess Your Business and Infrastructure Readiness for the Ultimate Technological Leap.",
-      image: "/Image/fields/Maskgroup-2.jpg",
+      image: "/Image/casestudy/3.jpg",
     },
+
+    {
+        sector: "Healthcare Operations.",
+        category: "Web Service",
+        published: "2 Min Read",
+        title: "Transforming Healthcare Operations with Amazon Web Services",
+        description:
+          "We're Gen AI-Ready and Eager to Collaborate. Let's Assess Your Business and Infrastructure Readiness for the Ultimate Technological Leap.Readiness for the Ultimate Technological Leap.",
+        image: "/Image/casestudy/4.jpg",
+      },
+      {
+        sector: "AI Chatbots",
+        category: "AI Chatbots",
+        published: "2 Min Read",
+        title: "Revolutionizing Customer Service with AWS-Powered AI Chatbots for a Leading Retail Chain",
+        description:
+          "In collaboration with Cloud Pro AI, the Company embarked on a journey to overhaul its custom. Readiness for the Ultimate Technological Leap.Readiness for the Ultimate Technological Leap.",
+        image: "/Image/casestudy/5.jpg",
+      },
+      {
+        sector: "Billing and Compliance with Cloud Pro AI",
+        category: "Web Service",
+        published: "2 Min Read",
+        title: "Transforming Healthcare Billing and Compliance with Cloud Pro AI and Amazon Web Services",
+        description:
+          "We're Gen AI-Ready and Eager to Collaborate. Let's Assess Your Business and Infrastructure Readiness for the Ultimate Technological Leap.",
+        image: "/Image/casestudy/6.jpg",
+      },
+
+
+      {
+        sector: "Healthcare Operations.",
+        category: "Web Service",
+        published: "2 Min Read",
+        title: "Transforming Healthcare Operations with Amazon Web Services",
+        description:
+          "We're Gen AI-Ready and Eager to Collaborate. Let's Assess Your Business and Infrastructure Readiness for the Ultimate Technological Leap.Readiness for the Ultimate Technological Leap.",
+        image: "/Image/casestudy/7.jpg",
+      },
+      {
+        sector: "AI Chatbots",
+        category: "AI Chatbots",
+        published: "2 Min Read",
+        title: "Revolutionizing Customer Service with AWS-Powered AI Chatbots for a Leading Retail Chain",
+        description:
+          "In collaboration with Cloud Pro AI, the Company embarked on a journey to overhaul its custom. Readiness for the Ultimate Technological Leap.Readiness for the Ultimate Technological Leap.",
+        image: "/Image/casestudy/8.jpg",
+      },
+      {
+        sector: "Billing and Compliance with Cloud Pro AI",
+        category: "Web Service",
+        published: "2 Min Read",
+        title: "Transforming Healthcare Billing and Compliance with Cloud Pro AI and Amazon Web Services",
+        description:
+          "We're Gen AI-Ready and Eager to Collaborate. Let's Assess Your Business and Infrastructure Readiness for the Ultimate Technological Leap.",
+        image: "/Image/casestudy/9.jpg",
+      },
   ];
   

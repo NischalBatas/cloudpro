9e1b5eb9a7b2d57ee3ad39746e731886cfd05825
Batas <nischal.batas88@gmail.com>
2024-05-07 12:08:32 +0545

    adding case study detail page

--- src/components/Page/CaseStudy/caseDetails/Content/BannerOne.js
+++ src/components/Page/CaseStudy/caseDetails/Content/BannerOne.js
@@ -3,14 +3,14 @@ import React from "react";
 const BannerOne = () => {
   return (
     <div
-      className="flex flex-col justify-end h-[420px] md:h-[504px]  bg-no-repeat bg-center bg-cover"
+      className="flex flex-col justify-end h-[250px] md:h-[399px]  bg-no-repeat bg-center bg-cover"
       style={{
         // filter: "blur(2px)",
         zIndex: "-1",
         backgroundImage: "url('/Image/casestudy/am.svg')",
       }}
     >
-      <p className="m-auto max-w-[839px] text-center font-bold text-[32px] leading-[48px]">
+      <p className="m-auto max-w-[839px] text-center font-bold text:[24px] md:text-[32px] md:leading-[48px]">
         Enabled real-time health analytics, processing data queries in minutes,
         dramatically improving operational efficiency and patient care
         responsiveness.
--- src/components/Page/CaseStudy/caseDetails/Content/Solution.js
+++ src/components/Page/CaseStudy/caseDetails/Content/Solution.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const Solution = ({ content }) => {
   return (
-    <div className="navbar-main4 py-20">
+    <div className="px-2 md:px-0 lg:navbar-main4 py-10 md:py-20">
       <div className="main-container2">
         <div>
           <p className="text-[24px] font-semibold">Solution</p>
           <p className="text-[14px] leading-6">Process Flow</p>
         </div>
-        <div className="flex justify-start gap-4 flex-wrap">
+        <div className="grid grid-cols-2 md:flex md:justify-start gap-2 md:gap-4 flex-wrap">
           {content.map((item, index) => {
             return (
              

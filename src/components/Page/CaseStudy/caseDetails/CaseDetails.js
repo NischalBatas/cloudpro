843b5fb236bdcfd6f7c5c0f69d83e92daad361b3
Batas <nischal.batas88@gmail.com>
2024-05-07 10:33:34 +0545

    adding case study detail page

--- src/components/Page/CaseStudy/caseDetails/CaseDetails.js
+++ src/components/Page/CaseStudy/caseDetails/CaseDetails.js
@@ -1,39 +1,12 @@
 import React from "react";
-import { FaRegClock } from "react-icons/fa";
-import { CiLinkedin } from "react-icons/ci";
-import { RiFacebookCircleLine } from "react-icons/ri";
-import { RiTwitterXLine } from "react-icons/ri";
-import Link from "next/link";
+
+import BreadCrumb from "./Content/BreadCrumb";
+import Introduction from "./Content/Introduction";
 const CaseDetails = () => {
   return (
   <div>
-      <div className="flex flex-col justify-end h-[420px] md:h-[504px]  bg-no-repeat bg-center bg-cover"   style={{
-          // filter: "blur(2px)",
-          zIndex: "-1",
-          backgroundImage: "url('/Image/casestudy/bg1.webp')"}}
-        >
-     <div className="navbar-main">
-    <div className="main-container2">
-    <div className="pt-12">
-        <p className="text-[14px] text-[#6083F3] font-semibold uppercase tracking-wider">CaseStudy</p>
-        <p className="text-[24px] md:text-[32px] font-semibold max-w-[615px]">
-          Transforming Healthcare Operations with Amazon Web Services
-        </p>
-      </div>
-      <div className="flex items-center justify-between mt-28 pb-2">
-        <div className="flex gap-6 text-[14px] text-[#BDBDBD]">
-            <span>May 6, 2023</span>
-            <span className="flex items-center gap-1"><FaRegClock /> 5 min Read</span>
-        </div>
-        <div className="flex gap-5 text-[22px]">
-      <Link href='/' className="hover:bg-white hover:text-[#0073ae] rounded-md"><CiLinkedin /></Link>
-      <Link href='/' className="hover:bg-white hover:text-[#0469de] rounded-full"><RiFacebookCircleLine /></Link>
-      <Link href='/' className="hover:text-[black] "><RiTwitterXLine /></Link>
-        </div>
-      </div>
-    </div>
-     </div>
-    </div>
+      <BreadCrumb/>
+      <Introduction/>
   </div>
   );
 };
---
+++ src/components/Page/CaseStudy/caseDetails/Content/Introduction.js
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import React from "react";
+
+const Introduction = () => {
+  return (
+    <div className="navbar-main bg-white">
+      <div className="main-container flex justify-between items-center flex-wrap md:flex-nowrap py-8 md:py-12 gap-x-2 gap-y-4">
+        <div className="md:max-w-[444px]">
+          <p className="text-black text-[24px] font-semibold">Introduction</p>
+          <p className="text-[16px] text-black leading-8 mt-2">
+            In the highly regulated and complex field of healthcare billing and
+            compliance, efficiency and accuracy are paramount. A prominent
+            healthcare services provider, grappling with the challenges of
+            managing a high volume of audit requests and maintaining revenue
+            integrity, turned to Cloud Pro AI for a solution. Our mission was to
+            leverage Amazon Web Services (AWS) to automate and streamline their
+            audit workflow, significantly reducing manual effort and improving
+            response accuracy.
+          </p>
+        </div>
+        <div>
+          <Image
+            className="min-w-[350px] rounded-md"
+            width={510}
+            height={308}
+            src="/Image/caseStudy/bg2.png"
+            alt="Case details"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Introduction;

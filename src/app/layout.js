4399557343ee7ed1b020f5c25b4abd4b9fcf997c
Batas <nischal.batas88@gmail.com>
2024-05-07 21:44:44 +0545

    home background change

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">  
     <body className={inter.className}>
-
+    <link rel="shortcut icon" href="/favicon.ico" />
         {children}
         <Script src="../../static/js/jquery.js"></Script>
         <Script src="../../static/js/script.js"></Script>

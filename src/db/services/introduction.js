5e38c18e52901ed5fd0e86b379304417a0e1f318
Batas <nischal.batas88@gmail.com>
2024-05-07 19:43:48 +0545

    home background change

--- src/db/services/introduction.js
+++ src/db/services/introduction.js
@@ -1,6 +1,6 @@
 export const introductionAdhoc = [
   {
-    "path": "/ad-hoc-reporting",
+    "path": "ad-hoc-reporting",
     "title": "Ad-hoc Reporting Solutions",
     "description": "Enable Dynamic Decision-Making with Ad-hoc Reporting Solutions.\nEmpower your business users with Cloudproai’s Ad-hoc Reporting Solutions, allowing them to create customized, timely reports based on their immediate needs. This flexibility ensures that decision-makers have the most relevant data at their fingertips, facilitating quick responses and informed decisions in a dynamic business environment. Discover the power of flexible data exploration and real-time analytics with Cloudproai’s Ad-hoc Reporting Solutions.",
     "button": "Customize Your Reports",
@@ -11,7 +11,7 @@ export const introductionAdhoc = [
 
 export const introductionaiConsulting = [
   {
-    "path": "/ai-consulting",
+    "path": "ai-consulting",
     "title": "AI Consulting",
     "description": "Empower Your Business with Expert AI Consulting Services.\nIn today's competitive and rapidly evolving digital landscape, businesses of all sizes are on a quest for innovative solutions that can propel them ahead of the curve. At the heart of this quest is generative AI, a transformative force in artificial intelligence that offers unparalleled creativity and efficiency. Cloud ProAI stands as a beacon in this journey, enabling businesses to harness the power of generative AI apps to foster originality, automate processes, and personalize customer experiences at an unprecedented scale.",
     "button": "Partner with Us",
@@ -21,7 +21,7 @@ export const introductionaiConsulting = [
 
 export const introductionarchitectureConsulting = [
   {
-    "path": "/architecture-consulting",
+    "path": "architecture-consulting",
     "title": "Architecture Consulting",
     "description": "Optimize Your Cloud Infrastructure with Expert Architecture Consulting.\nElevate your cloud operations with strategic architecture consulting from Cloudproai. Our team of cloud experts will guide you through designing, optimizing, and scaling your cloud infrastructure to meet your business needs efficiently and effectively. Transform your cloud strategy, reduce costs, and enhance scalability with Cloudproai’s comprehensive architecture consulting services.",
     "button": "Consult with Us",
@@ -32,7 +32,7 @@ export const introductionarchitectureConsulting = [
 
 export const introductionbigDataConsulting = [
   {
-    "path": "/big-data-consulting",
+    "path": "big-data-consulting",
     "title": "Big Data Consulting",
     "description": "Unlock the Power of Big Data with Expert Consulting.\nHarness the full potential of your data assets with Cloudproai’s Big Data consulting services. Our team of experts assists in developing strategies that transform complex data sets into actionable insights, driving innovation and competitive advantage in your market. Enhance decision-making, optimize operations, and foster innovation with Cloudproai’s comprehensive Big Data consulting services.",
     "button": "Empower Your Data",
@@ -42,7 +42,7 @@ export const introductionbigDataConsulting = [
 
 export const introductionbiReportDashboard = [
   {
-    "path": "/bi-report-dashboard",
+    "path": "bi-report-dashboard",
     "title": "BI Report & Dashboard",
     "description": "Empower Decision-Making with Custom BI Reports and Dashboards.\nTransform your data into actionable insights with Cloudproai’s BI Report and Dashboard services. Our solutions provide powerful visualization tools and custom reports that allow stakeholders to grasp complex data quickly and make informed decisions effectively. Optimize your strategic planning and performance monitoring with clear, impactful data presentations. Drive better business outcomes with enhanced data visualization and analytics capabilities from Cloudproai.",
     "button": "Visualize Your Success",
@@ -53,7 +53,7 @@ export const introductionbiReportDashboard = [
 
 export const introductioncloudOps = [
   {
-    "path": "/cloudops",
+    "path": "cloudops",
     "title": "CloudOps",
     "description": "Streamline Your Data Operations with Advanced ETL Services.\nMaster your data landscape with Cloudproai’s ETL services, designed to efficiently extract, transform, and load your data, ensuring it's ready for analysis and decision-making. Optimize your data workflows to support real-time intelligence and operational efficiency. Enhance your data management, increase accessibility, and drive better business outcomes with Cloudproai’s expert ETL services.",
     "button": "Transform Your Data Today",
@@ -63,7 +63,7 @@ export const introductioncloudOps = [
 
 export const introductiondataVisualizationSolutions = [
   {
-    "path": "/data-visualization-solutions",
+    "path": "data-visualization-solutions",
     "title": "Data Visualization Solutions",
     "description": "Bring Your Data to Life with Advanced Data Visualization Solutions.\nElevate your analytics with Cloudproai’s Data Visualization Solutions. Our team empowers organizations to visualize complex datasets in an intuitive format, making it easier to identify trends, patterns, and outliers. By enhancing the way you see data, we help you make informed decisions quickly and effectively. Transform data into visually engaging insights with Cloudproai’s cutting-edge Data Visualization Solutions.",
     "button": "Visualize Now",
@@ -74,7 +74,7 @@ export const introductiondataVisualizationSolutions = [
 
 export const introductiondataPipelineSolutions = [
   {
-    "path": "/data-pipeline-solutions",
+    "path": "data-pipeline-solutions",
     "title": "Data Pipeline Solutions",
     "description": "Unlock the Power of Big Data with Expert Consulting.\nStreamline Your Data Workflows with Advanced Data Pipeline Solutions. Optimize your data operations with Cloudproai’s Data Pipeline Solutions, designed to ensure efficient, reliable, and scalable data flow across your entire analytics ecosystem. Our expert team configures and manages data pipelines that empower your business to process and analyze data in real-time, supporting informed decision-making and operational excellence. Enhance your data infrastructure for peak performance and flexibility with Cloudproai’s specialized Data Pipeline Solutions.",
     "button": "Enhance Your Data Flow",
@@ -85,7 +85,7 @@ export const introductiondataPipelineSolutions = [
 
 export const introductiondataAnalytics = [
   {
-    "path": "/data-analytics",
+    "path": "data-analytics",
     "title": "Data Analytics",
     "description": "Transform Data into Decisions with Advanced Analytics.\nLeverage Cloudproai’s Data Analytics services to uncover valuable insights from your data. Our experts employ cutting-edge techniques and tools to analyze, interpret, and visualize data, helping you make informed decisions that propel your business forward. Make smarter decisions, optimize operations, and innovate with Cloudproai’s comprehensive Data Analytics services.",
     "button": "Analyse Your Data",
@@ -96,7 +96,7 @@ export const introductiondataAnalytics = [
 
 export const introductiondataLakesWarehouses = [
   {
-    "path": "/data-lakes-warehouses",
+    "path": "data-lakes-warehouses",
     "title": "Data Lakes and Warehouses",
     "description": "Build the Foundation for Advanced Analytics with Data Lakes and Warehouses.\nEmpower your business with Cloudproai’s Data Lakes and Warehouses services, designed to centralize and structure massive amounts of data for complex analysis and business intelligence. Our solutions enable scalable, secure, and efficient data storage that supports real-time decision-making and data-driven innovation. Streamline your data operations and unlock powerful insights with Cloudproai’s expert Data Lakes and Warehouses services.",
     "button": "Optimize Your Data Storage",
@@ -106,7 +106,7 @@ export const introductiondataLakesWarehouses = [
 
 export const introductionetl = [
   {
-    "path": "/etl",
+    "path": "etl",
     "title": "ETL",
     "description": "Maximize Efficiency with Proactive CloudOps Services.\nElevate your cloud operations with Cloudproai’s CloudOps services, ensuring continuous optimization, reliability, and security of your cloud infrastructure. Our expert team empowers your business to thrive in the cloud, reducing overheads and boosting performance. Drive operational excellence and ensure high availability with Cloudproai’s expert CloudOps services.",
     "button": "Optimize Your Cloud",
@@ -116,7 +116,7 @@ export const introductionetl = [
 
 export const introductiongenerativeAI = [
   {
-    "path": "/generative-ai",
+    "path": "generative-ai",
     "title": "Generative AI",
     "description": "In today's competitive and rapidly evolving digital landscape, businesses of all sizes are on a quest for innovative solutions that can propel them ahead of the curve. At the heart of this quest is generative AI, a transformative force in artificial intelligence that offers unparalleled creativity and efficiency. Cloud ProAI stands as a beacon in this journey, enabling businesses to harness the power of generative AI apps to foster originality, automate processes, and personalize customer experiences at an unprecedented scale.",
     "button": "Speak to an AI Expert",
@@ -127,7 +127,7 @@ export const introductiongenerativeAI = [
 
 export const introductiongenerativeAIApplications = [
   {
-    "path": "/generative-ai-applications",
+    "path": "generative-ai-applications",
     "title": "Generative AI and Applications",
     "description": "Discover the power of Generative AI based applications in your industry. Equip your technology infrastructure with state-of-the-art LLM models to efficiently manage customer interaction, document flow, and process automation. Accelerate your business growth potential, adopt innovative solutions, and unlock new avenues of revenue growth with Cloudpro AI's Generative AI solutions.",
     "button": "Build with Us",
@@ -138,7 +138,7 @@ export const introductiongenerativeAIApplications = [
 
 export const introductioninfrastructureMigrationUpgrades = [
   {
-    "path": "/infrastructure-migration-upgrades",
+    "path": "infrastructure-migration-upgrades",
     "title": "Infrastructure Migration and Upgrades",
     "description": "Seamless Infrastructure Migration and Upgrades to the Cloud.\nNavigate the complexities of cloud migration with Cloudproai’s expert services. From initial assessment to final optimization, our team ensures a smooth transition and strategic upgrade of your infrastructure to the cloud, aligning with your business objectives for enhanced scalability, performance, and cost-efficiency. Accelerate your cloud transformation with minimal disruption and maximum benefits with Cloudproai’s migration and upgrade services.",
     "button": "Start Your Cloud Journey",

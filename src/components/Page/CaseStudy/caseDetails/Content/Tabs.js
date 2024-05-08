b3c0cbf3fc7e1455d42fef8d0c266611c1a1e2a6
Batas <nischal.batas88@gmail.com>
2024-05-08 13:18:26 +0545

    adding case study detail page

--- src/components/Page/CaseStudy/caseDetails/Content/Tabs.js
+++ src/components/Page/CaseStudy/caseDetails/Content/Tabs.js
@@ -4,8 +4,8 @@ import React from 'react'
 const Tabs = (props) => {
     console.log("Hello")
   return (
-    <div className='navbar-main tabs_main_service bg-white text-[#6A6A6A]  border-b-[1px]'>
-        <div className='main-container flex justify-start gap-[8rem] flex-wrap'>
+    <div className='navbar-main tabs_main_service bg-white text-[#6A6A6A] text-[14px] md:text-[16px]  border-b-[1px]'>
+        <div className='main-container flex justify-between gap-6 text-nowrap overflow-x-scroll'>
         <Link className='py-5' href='#introduction_section'>Introduction</Link>
         <Link className='py-5' href='#solution_section'>Solution</Link>
         <Link className='py-5' href='#technology_section'>Technology</Link>

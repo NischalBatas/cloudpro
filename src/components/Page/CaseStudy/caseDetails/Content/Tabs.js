d1e81d57e5eb263cf265cdb8a1cdc90e41ff116a
Batas <nischal.batas88@gmail.com>
2024-05-08 12:58:30 +0545

    adding case study detail page

--- src/components/Page/CaseStudy/caseDetails/Content/Tabs.js
+++ src/components/Page/CaseStudy/caseDetails/Content/Tabs.js
@@ -4,13 +4,13 @@ import React from 'react'
 const Tabs = (props) => {
     console.log("Hello")
   return (
-    <div className='navbar-main bg-white text-[#6A6A6A] py-5 border-b-[1px] font-normal'>
+    <div className='navbar-main tabs_main_service bg-white text-[#6A6A6A]  border-b-[1px]'>
         <div className='main-container flex justify-start gap-[8rem] flex-wrap'>
-        <Link href='#introduction_section'>Introduction</Link>
-        <Link href='#solution_section'>Solution</Link>
-        <Link href='#technology_section'>Technology</Link>
-        <Link href='#impactandResults_section'>Impact and Results</Link>
-        <Link href='#conclusion_section'>Conclusion</Link>
+        <Link className='py-5' href='#introduction_section'>Introduction</Link>
+        <Link className='py-5' href='#solution_section'>Solution</Link>
+        <Link className='py-5' href='#technology_section'>Technology</Link>
+        <Link className='py-5' href='#impactandResults_section'>Impact and Results</Link>
+        <Link className='py-5' href='#conclusion_section'>Conclusion</Link>
         </div>
     </div>
   )

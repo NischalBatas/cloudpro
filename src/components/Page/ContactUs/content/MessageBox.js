f31adf77b061a2f010c1249e79d06666729d7741
Batas <nischal.batas88@gmail.com>
2024-05-10 09:43:54 +0545

    changing navbar color

--- src/components/Page/ContactUs/content/MessageBox.js
+++ src/components/Page/ContactUs/content/MessageBox.js
@@ -4,7 +4,7 @@ import * as Form from '@radix-ui/react-form';
 import Image from "next/image";
 import Calendar from './Calendar';
 const MessageBox = () => {
-  const [form,setForm]=useState({
+  const [formData,setFormData]=useState({
     fullName:'',
     email:'',
     phoneNumber:'',
@@ -13,8 +13,37 @@ const MessageBox = () => {
 
   const handleChange=(e)=>{
     const {name,value}=e.target
-    setForm({...form,[name]:value})
+    setFormData({...formData,[name]:value})
   }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const res = await emailjs.send(
+        service_0o2pexp,
+        template_vf4lvgo,
+        formData,
+        x3ekMbtD_O9TeGs5y
+      );
+  
+      if (res.status === 200) {
+        alert("Sent Successfully, We will get back to you soon.");
+  
+        // Reset the formData fields
+        setFormData({
+          fullName: '',
+          phoneNumber: '',
+          email: '',
+          message: '',
+        });
+      }
+    } catch (error) {
+      console.log("EmailJS error:", error);
+      alert("Can't send, please check and try again.");
+    }
+  };
+  
 
   return (
     <Tabs.Root className="TabsRoot1 mt-8 md:mt-0" defaultValue="tab2">
@@ -28,7 +57,7 @@ const MessageBox = () => {
     </Tabs.List>
     <Tabs.Content className="TabsContent1" value="tab1">
         
-    <Form.Root className="FormRoot">
+    <Form.Root className="FormRoot" onSubmit={handleSubmit}>
 <Form.Field className="FormField" name="Your full name">
   <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
     
@@ -41,7 +70,7 @@ const MessageBox = () => {
     </Form.Message>
   </div>
   <Form.Control asChild>
-    <input value={form.fullName} name='fullname' onChange={handleChange}  className="Input" type="text" required  placeholder="eg.John Smith"/>
+    <input value={formData.fullName} name='fullName' onChange={handleChange}  className="Input" type="text" required  placeholder="eg.John Smith"/>
   </Form.Control>
 </Form.Field>
 
@@ -57,12 +86,12 @@ const MessageBox = () => {
     </Form.Message>
   </div>
   <Form.Control asChild>
-    <input value={form.email} name='email' onChange={handleChange} className="Input" type="email" required placeholder="eg.you@example.com"/>
+    <input value={formData.email} name='email' onChange={handleChange} className="Input" type="email" required placeholder="eg.you@example.com"/>
   </Form.Control>
 </Form.Field>
 
 
-<Form.Field  className="FormField" name="Services">
+{/* <Form.Field  className="FormField" name="Services">
 <Form.Label className="FormLabel">Select all services you may need.</Form.Label>
 <Form.Control asChild>
 <select className="p-2 font-semibold rounded-md bg-[#252525]">
@@ -74,7 +103,7 @@ const MessageBox = () => {
   <option value="AI">App Development </option>
 </select>
 </Form.Control>
-</Form.Field>
+</Form.Field> */}
 
 <Form.Field className="FormField" name="phonenumber">
   <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
@@ -88,7 +117,7 @@ const MessageBox = () => {
     </Form.Message>
   </div>
   <Form.Control asChild>
-    <input value={form.phoneNumber} name='phoneNumber' onChange={handleChange} className="Input" type="number" required  placeholder="Enter your phone number"/>
+    <input value={formData.phoneNumber} name='phoneNumber' onChange={handleChange} className="Input" type="number" required  placeholder="Enter your phone number"/>
   </Form.Control>
 </Form.Field>
 
@@ -100,12 +129,12 @@ const MessageBox = () => {
     </Form.Message>
   </div>
   <Form.Control asChild>
-    <textarea value={form.message} name='message' onChange={handleChange} className="Textarea" required />
+    <textarea value={formData.message} name='message' onChange={handleChange} className="Textarea" required />
   </Form.Control>
 </Form.Field>
 
 <Form.Submit asChild>
-  <button className="Button bg-cloud" style={{ marginTop: 10 }}>
+  <button type='submit' className="Button bg-cloud" style={{ marginTop: 10 }}>
   Send Message
   </button>
 </Form.Submit>
@@ -119,10 +148,9 @@ const MessageBox = () => {
 
 
 
-    <Tabs.Content className="TabsContent1" value="tab2">
+    {/* <Tabs.Content className="TabsContent1" value="tab2">
       <Calendar/>
-      {/* <Image className="mb-10" width={500} height={500} src='/Image/phone.jpg' alt="phone_image"/> */}
-      <Form.Root className="FormRoot mt-6">
+            <Form.Root className="FormRoot mt-6">
 <Form.Field className="FormField" name="Your full name">
   <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
     
@@ -197,7 +225,7 @@ const MessageBox = () => {
   </button>
 </Form.Submit>
 </Form.Root>
-    </Tabs.Content>
+    </Tabs.Content> */}
   </Tabs.Root>
   )
 }

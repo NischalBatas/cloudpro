c8f6e3664ce25a2eef2291cdcad45235b9bb0ecd
Batas <nischal.batas88@gmail.com>
2024-05-14 19:14:18 +0545

    arranging footer

--- src/components/Footer/FooterCopyRight.js
+++ src/components/Footer/FooterCopyRight.js
@@ -3,17 +3,19 @@ import React from "react";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
+import Image from "next/image";
 
 const FooterCopyRight = () => {
   return (
     <div className="footer_width flex justify-between items-center gap-2 flex-wrap pt-4">
-      <div className="flex  gap-4 text-cloud2 text-[10px] md:text-sm">
+      <div className="flex flex-wrap gap-4 md:gap-8 text-cloud2 text-[10px] md:text-[14px]">
+        <Link href='/'><Image width={124} height={14.289} src='/Image/logo/logo-white.svg' alt="footer_logo" /></Link>
         <Link href="#" className="font-semibold">2024 CloudPro.AI</Link>
         <Link href="#">Privacy Policy</Link>
         <Link href="#">Terms of Sevices</Link>
       </div>
 
-      <div className="flex  gap-4 text-cloud2 text-[16px]">
+      <div className="flex  gap-[18px] text-cloud2 text-[24px]">
         <Link href="#"><FaXTwitter /></Link>
         <Link href="#"><FaLinkedin /></Link>
         <Link href="#"><FaInstagram /></Link>

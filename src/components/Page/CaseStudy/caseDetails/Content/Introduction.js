b12fe3395d8967d4ecd75c13c897ded004892eca
Batas <nischal.batas88@gmail.com>
2024-05-16 00:31:27 +0545

    complete dynamic content for case study

--- src/components/Page/CaseStudy/caseDetails/Content/Introduction.js
+++ src/components/Page/CaseStudy/caseDetails/Content/Introduction.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Introduction = ({caseData}) => {
   return (
-    <div className="navbar-main bg-white py-10" id='introduction_section'>
+    <div className="navbar-main bg-white py-6" id='introduction_section'>
       <div className="main-container flex justify-between items-center flex-wrap md:flex-nowrap  gap-x-2 gap-y-4">
         <div className="md:max-w-[444px]">
           <p className="text-black text-[24px] font-semibold">Introduction</p>

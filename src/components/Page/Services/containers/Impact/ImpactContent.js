41b4e1acd7f561eba57587b7a09e3d05fc9bcf27
Batas <nischal.batas88@gmail.com>
2024-05-15 17:56:11 +0545

    removing num

--- src/components/Page/Services/containers/Impact/ImpactContent.js
+++ src/components/Page/Services/containers/Impact/ImpactContent.js
@@ -7,7 +7,8 @@ const ImpactContent = ({content}) => {
     {content.data.map((item, index) => {
       return (
         <div key={index} className="testominal max-w-44 md:max-w-64 py-2 md:p-1">
-          <div className="text-2xl md:text-4xl font-semibold flex gap-1 items-end"><div className="text-[40px]">{item.ratio}</div>%</div>
+          {/* data-val={`${item.ratio}`} */}
+          <div className="text-2xl md:text-4xl font-semibold flex gap-1 items-end"><div className="text-[40px]"><span className="text-[32px]  md:text-[64px]" > {item.ratio}</span></div>%</div>
           <div className="mt-2 md:mt-4 text-[14px]">
             <span className="font-medium capitalize mr-1">{item.status}</span>
             <span className="text-[#5B5B5B]">{item.description}</span>

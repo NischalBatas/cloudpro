768feef31e93cd6cf52e49d60c7dc4f77bb8e2a6
Batas <nischal.batas88@gmail.com>
2024-05-17 12:15:28 +0545

    added skeleton for case detail page

--- src/components/Page/CaseStudy/caseDetails/Content/BannerTwo.js
+++ src/components/Page/CaseStudy/caseDetails/Content/BannerTwo.js
@@ -4,14 +4,14 @@ import ImpactAndResultSlider from "@/components/SplideSlider/ImpactAndResultSlid
 const BannerTwo = ({ content,caseData }) => {
   return (
     <div id="impactandResults_section"
-      className="flex flex-col items-center justify-center text-center h-[250px] md:h-[375px]  bg-no-repeat bg-center bg-cover"
+      className="flex flex-col items-center justify-center text-center min-h-[250px] md:min-h-[375px]  bg-no-repeat bg-center bg-cover"
       style={{
         // filter: "blur(2px)",
         zIndex: "-1",
         backgroundImage: "url('/Image/casestudy/ga.svg')",
       }}
     >
-      <p className="mb-10 tracking-wider uppercase pb-2 text-[14px] border-b-[1px] border-[#b4b4b4]">
+      <p className=" tracking-wider uppercase pb-2 text-[14px] border-b-[1px] border-[#b4b4b4]">
         Impact and Results
       </p>
         <ImpactAndResultSlider caseData={caseData} content={content}/>

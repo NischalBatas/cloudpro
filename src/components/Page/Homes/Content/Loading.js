9118baed5492f2bfd4bfba1c94942e122d270118
Batas <nischal.batas88@gmail.com>
2024-05-17 21:37:49 +0545

    adding casestudy content in the home page through api

--- src/components/Page/Homes/Content/Loading.js
+++ src/components/Page/Homes/Content/Loading.js
@@ -1,9 +1,79 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
   return (
-    <div>Loading...</div>
-  )
-}
+    <div className="main-container py-5 md:py-14">
+    <div className="mb-8 w-full md:w-8/12 lg:w-4/12 text-left">
+      <div className="font-semibold text-[32px]">Case Studies</div>
+      {/* <div className="text-cloud3 mt-3 text-sm">
+        We're Gen AI-Ready and Eager to Collaborate. Let's Assess Your
+        Business and Infrastructure Readiness for the Ultimate Technological
+        Leap.
+      </div> */}
+    </div>
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3 my-6">
+           <div>
+              <div className="md:max-w-[324px] casestudy_product_container">
+                <div className="h-[202px] md:max-w-[324px] bg-[#27292d] relative rounded-lg"></div>
+  
+                <div className="flex gap-2 items-center  mt-3 ">
+                  <div className="w-[100px] h-[16px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+                  <div className="w-[60px] h-[14px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+                </div>
+  
+                <div className="my-4">
+                  <div className="w-[310px] h-[20px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+                  <div className="mt-3 w-[310px] h-[16px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+                  <div className="mt-2 w-[310px] h-[16px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+                  <div className="mt-2 w-[310px] h-[16px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+  
+                </div>
+              </div>
+            </div>
+  
+            <div>
+              <div className="md:max-w-[324px] casestudy_product_container">
+                <div className="h-[202px] md:max-w-[324px] bg-[#27292d] relative rounded-lg"></div>
+  
+                <div className="flex gap-2 items-center  mt-3 ">
+                  <div className="w-[100px] h-[16px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+                  <div className="w-[60px] h-[14px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+                </div>
+  
+                <div className="my-4">
+                  <div className="w-[310px] h-[20px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+                  <div className="mt-3 w-[310px] h-[16px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+                  <div className="mt-2 w-[310px] h-[16px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+                  <div className="mt-2 w-[310px] h-[16px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+  
+                </div>
+              </div>
+            </div>
 
-export default Loading
+            <div>
+              <div className="md:max-w-[324px] casestudy_product_container">
+                <div className="h-[202px] md:max-w-[324px] bg-[#27292d] relative rounded-lg"></div>
+  
+                <div className="flex gap-2 items-center  mt-3 ">
+                  <div className="w-[100px] h-[16px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+                  <div className="w-[60px] h-[14px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+                </div>
+  
+                <div className="my-4">
+                  <div className="w-[310px] h-[20px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+                  <div className="mt-3 w-[310px] h-[16px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+                  <div className="mt-2 w-[310px] h-[16px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+                  <div className="mt-2 w-[310px] h-[16px] animate-pulse bg-[#27292d] rounded-[6px]"></div>
+  
+                </div>
+              </div>
+            </div>
+  
+  
+            
+    </div>
+    </div>
+  )
+};
+
+export default Loading;

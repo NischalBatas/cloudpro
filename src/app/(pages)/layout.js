bbe26c7d5be0583cb081873e6ed2b4dd113af44e
Batas <nischal.batas88@gmail.com>
2024-05-21 19:14:49 +0545

    add

--- src/app/(pages)/layout.js
+++ src/app/(pages)/layout.js
@@ -1,13 +1,13 @@
-
+"use client"
 import "@radix-ui/themes/styles.css";
 import Header from "@/components/Header/Header";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
-// import ScrollToTop from "react-scroll-to-top";
+import ScrollToTop from "react-scroll-to-top";
 import { FaArrowUp } from "react-icons/fa";
 import MobileNavigation from "@/components/Navbar/MobileMenu/MobileNavigation";
-// import ProgressBar from "@/components/Animation/ProgressBar";
-// import { Suspense } from "react";
+import ProgressBar from "@/components/Animation/ProgressBar";
+import { Suspense } from "react";
 
 
 
@@ -15,20 +15,20 @@ import MobileNavigation from "@/components/Navbar/MobileMenu/MobileNavigation";
 const ProtectedLayout = ({ children }) => {
   return (
 
- <>
+ <Suspense>
 
 
-{/* <ProgressBar/> */}
-   {/* <Header /> */}
+<ProgressBar/>
+   <Header />
    <Navbar />
    <MobileNavigation/>
   
      {children}
 
-     {/* <ScrollToTop top='40' component={<FaArrowUp />} className='scroll_top' smooth width='15' height="15"/> */}
+     <ScrollToTop top='40' component={<FaArrowUp />} className='scroll_top' smooth width='15' height="15"/>
     
   
- </>
+ </Suspense>
 
   );
 };

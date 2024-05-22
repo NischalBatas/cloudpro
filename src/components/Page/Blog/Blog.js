96234ba657fb422c4a86aa6f9f4f06d756073e47
Batas <nischal.batas88@gmail.com>
2024-05-22 19:58:12 +0545

    updating blog posts

--- src/components/Page/Blog/Blog.js
+++ src/components/Page/Blog/Blog.js
@@ -3,10 +3,9 @@ import React from "react";
 import { MdOutlineArrowOutward } from "react-icons/md";
 
 import Loading from "./Loading";
-import './blog.css'
+import "./blog.css";
 import Footer from "@/components/Footer/Footer";
 import Newsletter from "../CaseStudy/caseDetails/Content/Newsletter";
-
 
 async function getPosts() {
   const query = `query GetAllBlogPost {
@@ -43,81 +42,78 @@ async function getPosts() {
   return data.blogs.nodes;
 }
 
-
 const Blog = async () => {
-  const posts=await getPosts()
+  const posts = await getPosts();
   console.log("Blog Component - Post details", posts);
   return (
-   <>
-   {posts ? 
-    <div>
-    <div className="h-[420px] md:h-[504px]  pt-12 pb-12 text-center  flex flex-col justify-center items-center">
-      <p className="text-[32px] md:text-[48px] font-bold">Blogs</p>
-      <p className="py-2 md:p-0 text-[12px] md:text-[14px] md:leading-6 max-w-[514px] text-[#bebebe]">
-        We're Gen AI-Ready and Eager to Collaborate. Let's Assess Your
-        Business and Infrastructure Readiness for the Ultimate Technological
-        Leap.
-      </p>
-    </div>
+    <>
+      {posts ? (
+        <div>
+          <div className="h-[420px] md:h-[504px]  pt-12 pb-12 text-center  flex flex-col justify-center items-center">
+            <p className="text-[32px] md:text-[48px] font-bold">Blogs</p>
+            <p className="py-2 md:p-0 text-[12px] md:text-[14px] md:leading-6 max-w-[514px] text-[#bebebe]">
+              We're prepared for the era of Generative AI and excited to
+              collaborate. Let's evaluate your business and infrastructure to
+              ensure you're ready for this significant technological
+              advancement.
+            </p>
+          </div>
 
-    <div className="navbar-main bg-white ">
-      <div className="main-container relative bottom-28  grid md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-y-8 my-6">
-        {posts.map((item, index) => {
-          return (
-         <div  key={index}>
-             <Link href={`/blog/${item.slug}`} className="">
-              <div
-               
-                className="md:max-w-[324px] blogs_product_container"
-              >
-                <div
-                  className="h-[202px] md:max-w-[324px] relative  bg-no-repeat bg-cover rounded-lg"
-                  style={{
-                    backgroundImage: `url(${item.featuredImage.node.link})`,
-                  }}
-                >
-                  <div className="flex justify-between  px-4 pt-4  ">
-                    <div className="text-white text-base  font-medium w-44">
-                    {item.blogfield.blogcategory}
-                    </div>
-                    <div className="blogs_product_arrow">
-                      <MdOutlineArrowOutward className="blogs_product_arrow_icon rounded-full  w-6 h-6 text-xs p-1" />
-                    </div>
-                  </div>
-                </div>
+          <div className="navbar-main bg-white ">
+            <div className="main-container relative bottom-28  grid md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-y-8 my-6">
+              {posts.map((item, index) => {
+                return (
+                  <div key={index}>
+                    <Link href={`/blog/${item.slug}`} className="">
+                      <div className="md:max-w-[324px] blogs_product_container">
+                        <div
+                          className="h-[202px] md:max-w-[324px] relative  bg-no-repeat bg-cover rounded-lg"
+                          style={{
+                            backgroundImage: `url(${item.featuredImage.node.link})`,
+                          }}
+                        >
+                          <div className="flex justify-between  px-4 pt-4  ">
+                            <div className="text-white text-base  font-medium w-44">
+                              {item.blogfield.blogcategory}
+                            </div>
+                            <div className="blogs_product_arrow">
+                              <MdOutlineArrowOutward className="blogs_product_arrow_icon rounded-full  w-6 h-6 text-xs p-1" />
+                            </div>
+                          </div>
+                        </div>
 
-                <div className="flex gap-2 items-center text-xs mt-3 text-white">
-                  <div className="bg-cloud px-2 rounded-full py-1">
-                  {item.blogfield.blogcategory}
-                  </div>
-                  <div className="text-[10px] text-[#909090] tracking-wider">
-                  {item.blogfield.readTime} Read
-                  </div>
-                </div>
+                        <div className="flex gap-2 items-center text-xs mt-3 text-white">
+                          <div className="bg-cloud px-2 rounded-full py-1">
+                            {item.blogfield.blogcategory}
+                          </div>
+                          <div className="text-[10px] text-[#909090] tracking-wider">
+                            {item.blogfield.readTime} Read
+                          </div>
+                        </div>
 
-                <div className="my-4">
-                  <div className="blogs_product_title font-semibold text-[16px] text-[#000]">
-                    {item.title}
+                        <div className="my-4">
+                          <div className="blogs_product_title font-semibold text-[16px] text-[#000]">
+                            {item.title}
+                          </div>
+                          <div className="blogs_product_description mt-2 text-[14px] text-[#666666] leading-6">
+                            {item.blogfield.description}
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
                   </div>
-                  <div className="blogs_product_description mt-2 text-[14px] text-[#666666] leading-6">
-                  {item.blogfield.description}
-                  </div>
-                </div>
-              </div>
-            </Link>
-         </div>
-          );
-        })}
-  
-      </div>
-    </div>
+                );
+              })}
+            </div>
+          </div>
 
-    <Newsletter/>
-      <Footer/>
-
-  </div>
-  : (<Loading/>)}
-   </>
+          <Newsletter />
+          <Footer />
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 };
 

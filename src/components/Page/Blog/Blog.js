af653670cf53fab7265722058f216d54f0dec7fc
Batas <nischal.batas88@gmail.com>
2024-05-22 16:18:18 +0545

    updating blog

--- src/components/Page/Blog/Blog.js
+++ src/components/Page/Blog/Blog.js
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const Blog = async () => {
  
   return (
     <>
-      <h1>Blog</h1>
+      <h1>
+        <Link href='/blog/1'><div>Blog (Click Me)</div></Link>
+      </h1>
     
     </>
   );

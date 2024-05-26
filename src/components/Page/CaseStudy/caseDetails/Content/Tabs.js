605c1e76c5ff86387a7e1556812a3e232a80fbfd
Batas <nischal.batas88@gmail.com>
2024-05-26 22:05:06 +0545

    adding service content

--- src/components/Page/CaseStudy/caseDetails/Content/Tabs.js
+++ src/components/Page/CaseStudy/caseDetails/Content/Tabs.js
@@ -1,8 +1,34 @@
+"use client"
+import React, { useEffect } from 'react';
 import Link from 'next/link'
-import React from 'react'
 
 const Tabs = (props) => {
     console.log("Hello",props)
+    useEffect(() => {
+      const handleScroll = (event) => {
+          event.preventDefault();
+          const targetId = event.currentTarget.getAttribute('href').substring(1);
+          const targetElement = document.getElementById(targetId);
+
+          if (targetElement) {
+              window.scrollTo({
+                  top: targetElement.offsetTop - 50, // Adjust this value to account for the fixed navigation height
+                  behavior: 'smooth'
+              });
+          }
+      };
+
+      const links = document.querySelectorAll('a[href^="#"]');
+      links.forEach(link => {
+          link.addEventListener('click', handleScroll);
+      });
+
+      return () => {
+          links.forEach(link => {
+              link.removeEventListener('click', handleScroll);
+          });
+      };
+  }, []);
   return (
     <div className='navbar-main tabs_main_service bg-white text-[#6A6A6A] text-[14px] md:text-[16px]  border-b-[1px]'>
         <div className='main-container flex justify-between gap-6 text-nowrap overflow-x-auto'>

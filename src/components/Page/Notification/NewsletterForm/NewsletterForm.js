e07f9fda20165e424aa75edbe0a4e626c7692515
Batas <nischal.batas88@gmail.com>
2024-05-27 16:29:14 +0545

    adding newsletter form

--- src/components/Page/Notification/NewsletterForm/NewsletterForm.js
+++ src/components/Page/Notification/NewsletterForm/NewsletterForm.js
@@ -17,9 +17,9 @@ const NewsletterForm = () => {
         <div className="grid md:grid-cols-2 gap-10 md:gap-4">
         <div className="md:max-w-[424px]">
          <div className="max-w-[366px]">
-         <span className="max-w-[366px] text-[24px] md:text-[32px] font-bold">Get Notified with our newsletter</span>
+         <span className="max-w-[366px] text-[24px] md:text-[32px] leading-8 md:leading-[42px] font-bold">Get Notified with our newsletter</span>
          </div>
-             <p className="mt-1 md:mt-3  text-[16px] leading-[24px]">
+             <p className="mt-2 md:mt-3  text-[16px] leading-[24px]">
                Subscribe to our newsletter and stay up to date with the latest
                news, updates, and products. Get valuable insights into the world of
                AI

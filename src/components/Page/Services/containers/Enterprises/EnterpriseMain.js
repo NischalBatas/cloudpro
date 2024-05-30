ba7311ddc2ebe4ef14afb672c976d9ee14ae5407
Batas <nischal.batas88@gmail.com>
2024-05-30 10:08:31 +0545

    working newsletter qr

--- src/components/Page/Services/containers/Enterprises/EnterpriseMain.js
+++ src/components/Page/Services/containers/Enterprises/EnterpriseMain.js
@@ -2,13 +2,14 @@
 import React from 'react'
 import "./content/enterprises.css";
 import * as Tabs from "@radix-ui/react-tabs";
-
+import ServiceEnterprises from '@/components/SplideSlider/ServiceEnterprises';
+import './service_enterprises.css'
 
 
 const EnterpriseMain = ({content}) => {
   return (
     <div className="navbar-main py-16 ">
-    <div className="main-container ">
+    <div className="main-container hidden md:block">
       <Tabs.Root
         className="TabsRoot2 gap-6 lg:gap-4 flex-wrap md:flex-nowrap items-center overflow-hidden"
         defaultValue="tab1"
@@ -66,6 +67,41 @@ const EnterpriseMain = ({content}) => {
           );
         })}
       </Tabs.Root>
+    </div>
+
+    <div className='main-container md:hidden service_enterprises_mobileView'>
+        {/* <div className='flex'>
+        {content.data.map((item, index) => {
+          return (
+            <div key={index}>
+              <div className="relative">
+                <div
+                  className="absolute top-15 left-15 lg:w-[535px] lg:h-[383px] bg-cloud rounded"
+                  style={{ zIndex: "-1" }}
+                ></div>
+                <div className="max-w-[400px] lg:max-w-[535px] min-h-[383px] bg-white rounded text-black p-4">
+                  <p className="max-w-[381px] text-[16px] font-medium">
+                    {item.title}
+                  </p>
+                  <ul className="text-[#393939] text-[14px] mt-5 list-disc px-6 leading-7">
+                    {item.list.map((item, index) => {
+                      return (
+                        <li key={index} className="">
+                          {item}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+        </div> */}
+              <p className=" mb-4 font-medium text-[16px] max-w-[376px]">
+           {content.heading}
+          </p>
+        <ServiceEnterprises content={content}/>
     </div>
   </div>
   )
---
+++ src/components/SplideSlider/ServiceEnterprises.js
@@ -0,0 +1,83 @@
+import React from "react";
+import "@splidejs/splide/dist/css/themes/splide-default.min.css";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import Image from "next/image";
+import { testimonialClient } from "@/db/home/testimonialClient";
+import Link from "next/link";
+import { FaLinkedin } from "react-icons/fa";
+
+const ServiceEnterprises = ({content}) => {
+  const splideOptions = {
+    type: "loop",
+    perPage: 1,
+    perMove: 1,
+    rewind: true,
+    autoplay: false, // Whether to enable autoplay
+    interval: 2400, // Autoplay interval in milliseconds
+    pauseOnHover: true, // Whether to pause autoplay on hover
+    drag: true, // Whether to enable drag/swipe interaction
+    keyboard: true, // Whether to enable keyboard navigation
+    focus: "left",
+    lazyLoad: "sequential",
+    width: "100%",
+    // padding:'2rem',
+    padding: { right: "1rem" },
+    gap: "2rem",
+    pagination: false,
+    breakpoints: {
+      1332: {
+        // padding:'2rem',
+      },
+      1250: {
+        // padding:'1rem',
+      },
+      1125: {
+        perPage: 1,
+        padding: 0,
+      },
+      576: {
+        perPage: 1,
+          padding: 0,
+      },
+    },
+  };
+  return (
+    <div className="mx-auto overflow-hidden">
+      <Splide options={splideOptions}>
+
+        {content.data.map((item, index) => {
+          return (
+            <SplideSlide key={index}>
+              <div className="relative">
+                <div
+                  className="absolute top-15 left-15 lg:w-[535px] lg:h-[383px] bg-cloud rounded"
+                  style={{ zIndex: "-1" }}
+                ></div>
+                <div className=" lg:max-w-[535px] min-h-[383px] bg-white rounded text-black p-4">
+                  <p className="max-w-[381px] text-[16px] font-medium">
+                    {item.title}
+                  </p>
+                  <ul className="text-[#393939] text-[14px] mt-5 list-disc px-6 leading-7">
+                    {item.list.map((item, index) => {
+                      return (
+                        <li key={index} className="">
+                          {item}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </div>
+            </SplideSlide>
+          );
+        })}
+      
+        
+
+    
+      </Splide>
+    </div>
+  );
+};
+
+export default ServiceEnterprises;

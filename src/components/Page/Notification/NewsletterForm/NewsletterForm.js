19e08620ddf1b9c70345568f0af83d1fccd174d7
Batas <nischal.batas88@gmail.com>
2024-05-31 17:19:10 +0545

    newsletter

--- src/components/Page/Notification/NewsletterForm/NewsletterForm.js
+++ src/components/Page/Notification/NewsletterForm/NewsletterForm.js
@@ -1,52 +1,52 @@
-"use client"
+"use client";
 import Image from "next/image";
 import React from "react";
 import FormFields from "../FormFields/FormFields";
 import toast, { Toaster } from "react-hot-toast";
-
+import NewsletterSlider from "@/components/SplideSlider/NewsletterSlider";
+import './NewsletterForm.css'
 const NewsletterForm = () => {
-
   return (
-    <div className="">
-      <div
-        className="newsletter_main_content flex flex-col pt-24 pb-10   bg-no-repeat md:bg-center md:bg-cover"
-       
-      >
-  <div className="main-container4  md:bg-[black]  rounded-[16px] p-[24px] md:p-[55px]">
-        
-        <div className="grid md:grid-cols-2 gap-10 md:gap-4">
-        <div>
-        <div className="md:max-w-[424px]">
-         <div className="max-w-[366px]">
-         <span className="max-w-[366px] text-[24px] md:text-[32px] leading-8 md:leading-[42px] font-bold">Get Notified with our newsletter</span>
-         </div>
-             <p className="mt-2 md:mt-3  text-[16px] leading-[24px]">
-               Subscribe to our newsletter and stay up to date with the latest
-               news, updates, and products. Get valuable insights into the world of
-               AI
-             </p>
-           </div>
+    <div className="NewsletterForm_main">
+      <div className="newsletter_main_content flex flex-col pt-24 pb-10   bg-no-repeat md:bg-center md:bg-cover">
+        <div className="main-container4  md:bg-[black]  rounded-[16px] p-[24px] md:p-[55px]">
+          <div className="grid md:grid-cols-2 gap-10 md:gap-10">
+            <div>
+              <div className="md:max-w-[424px]">
+                <div className="max-w-[366px]">
+                  <span className="max-w-[366px] text-[24px] md:text-[32px] leading-8 md:leading-[42px] font-bold">
+                    Get Notified with our newsletter
+                  </span>
+                </div>
+                <p className="mt-2 md:mt-3  text-[16px] leading-[24px]">
+                  Subscribe to our newsletter and stay up to date with the
+                  latest news, updates, and products. Get valuable insights into
+                  the world of AI
+                </p>
+              </div>
 
-           <Toaster position="bottom-right"
-                                 toastOptions={{
-                                  style: {
-                                    fontSize:'14px', fontWeight:'600',background:'#fff'
-                                  }}}
-                                />
-           <div className="hidden md:flex items-center flex-col justify-center md:max-w-[424px]">
+              <Toaster
+                position="bottom-right"
+                toastOptions={{
+                  style: {
+                    fontSize: "14px",
+                    fontWeight: "600",
+                    background: "#fff",
+                  },
+                }}
+              />
+              {/* <div className="hidden md:flex items-center flex-col justify-center">
+         
            <Image className=" mt-10" width={200} height={200} src='/Image/notification/qr.png'/>
            <span className="mt-2 text-[12px] text-[#b0b0b0]">Please scan the QR code to access the form.</span>
-           </div>
-</div>
+           </div> */}
+              <NewsletterSlider />
+            </div>
 
-
-           <FormFields/>
+            <FormFields />
+          </div>
         </div>
-         </div>
-
       </div>
-
-    
     </div>
   );
 };
---
+++ src/db/newsletter/NewsletterData.js
@@ -0,0 +1,11 @@
+export const NewsletterData=
+[
+    {title:'Multi Platform Integration',image:'/Image/notification/1.svg'},
+    {title:'Easy Embed to Website',image:'/Image/notification/2.svg'},
+    {title:'Personalization',image:'/Image/notification/3.svg'},
+    {title:'Flow Design (Flow of Conversation)',image:'/Image/notification/4.svg'},
+    {title:'Support Ticket Automation (Any kind of API request)',image:'/Image/notification/5.svg'},
+    {title:'Voice/Text Channel',image:'/Image/notification/6.svg'},
+
+
+]

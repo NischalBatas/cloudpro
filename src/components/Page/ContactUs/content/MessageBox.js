95cdfa684d335480ddee721e5592ccd9a30173e3
Batas <nischal.batas88@gmail.com>
2024-06-02 00:15:15 +0545

    adding country code on phone number

--- src/components/Page/ContactUs/content/MessageBox.js
+++ src/components/Page/ContactUs/content/MessageBox.js
@@ -219,20 +219,7 @@ const options = [
           
           </Form.Field>
           
-          <Form.Field className="FormField " name="service">
-            <Form.Label className="FormLabel">
-            Select the services you may need.
-            <p className="text-[#7c7c7c] text-[12px] mt-[-10px] mb-[5px]"><span className="text-white font-bold">Note:</span> Press ctrl key to select multiple services</p>
-            </Form.Label>
-            <Form.Control asChild>
-            <select name="service" id="contactus_main_select" className="md:h-[302px] border p-2 sm:border-none rounded-[16px] bg-black text-white" multiple
-              onChange={handleChangeService} >
-            {options.map((option) => (
-              <option className="md:my-2 cursor-pointer md:p-2 md:w-[200px] md:text-center md:rounded-[16px] md:border" value={option.value}>{option.label}</option>
-            ))}
-          </select>
-            </Form.Control>
-          </Form.Field>
+       
 
           {/* <PhoneInput
                country={'us'}
@@ -301,6 +288,21 @@ const options = [
                 required
                 placeholder="Enter your Current Position"
               />
+            </Form.Control>
+          </Form.Field>
+
+          <Form.Field className="FormField " name="service">
+            <Form.Label className="FormLabel">
+            Select the services you may need.
+            <p className="text-[#7c7c7c] text-[12px] mt-[-10px] mb-[5px]"><span className="text-white font-bold">Note:</span> Press ctrl key to select multiple services</p>
+            </Form.Label>
+            <Form.Control asChild>
+            <select name="service" id="contactus_main_select" className="md:h-[302px] border p-2 sm:border-none rounded-[16px] bg-black text-white" multiple
+              onChange={handleChangeService} >
+            {options.map((option) => (
+              <option className="md:my-2 cursor-pointer md:p-2 md:w-[200px] md:text-center md:rounded-[16px] md:border" value={option.value}>{option.label}</option>
+            ))}
+          </select>
             </Form.Control>
           </Form.Field>
 

c1ad6d8e1835ee21973eccc6fd2e2f4ceb8ae60e
Batas <nischal.batas88@gmail.com>
2024-06-03 10:10:10 +0545

    adding middleware

---
+++ src/middleware.js
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server"
+
+
+export function middleware(request){
+// console.log('middlewareww')
+
+
+// Redirect to login when we access other page
+// if (request.nextUrl.pathname!=='/login'){
+// return NextResponse.redirect(new URL('/login',request.url))
+// }
+
+
+return NextResponse.redirect(new URL('/gavie-ai',request.url))
+}
+
+
+export const config={
+matcher:[
+'/newsletter'
+]
+}

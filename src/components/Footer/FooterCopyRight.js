c66acc218741c3605cbd2e94b5ddd23ebdea6983
Batas <nischal.batas88@gmail.com>
2024-06-07 19:03:32 +0545

    adding privacy page

--- src/components/Footer/FooterCopyRight.js
+++ src/components/Footer/FooterCopyRight.js
@@ -12,7 +12,7 @@ const FooterCopyRight = () => {
       <div className="flex flex-wrap items-center gap-3 md:gap-8 text-cloud2 text-[12px] md:text-[14px]">
         <Link href='/'><Image width={124} height={14.289} src='/Image/logo/logo-white.svg' alt="footer_logo" /></Link>
         <Link href="#" className="font-semibold">2024 CloudPro.AI</Link>
-        <Link href="#">Privacy Policy</Link>
+        <Link target="_blank" href="/privacy-policy">Privacy Policy</Link>
         <Link href="#">Terms of Sevices</Link>
       </div>
 

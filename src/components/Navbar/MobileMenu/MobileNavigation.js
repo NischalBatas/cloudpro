d9ed8e1f1dbdc70ff1773b25d5cf6835b978a1b9
Nischal Batas <nischal.batas88@gmail.com>
2024-06-18 22:59:02 +0545

    navbar changes

--- src/components/Navbar/MobileMenu/MobileNavigation.js
+++ src/components/Navbar/MobileMenu/MobileNavigation.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import Link from "next/link";
@@ -7,18 +7,34 @@ import MobileMenu from "./MobileMenu";
 import "./mobile.css";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { IoClose } from "react-icons/io5";
+
 const MobileNavigation = () => {
   const pathname = usePathname();
-  const [hamburger,setHamburger]=useState(false)
-  const [navbarStatus,setNavbarStatus]=useState(true)
+  const [hamburger, setHamburger] = useState(false);
+  const [navbarStatus, setNavbarStatus] = useState(true);
   const router = useRouter();
+  const navbarRef = useRef(null);
+
   const onHandleSubmit = (path) => {
     router.push(path);
   };
 
+  const handleClickOutside = (event) => {
+    if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+      setHamburger(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="hidden navbar-main-group navbar-main-group4 relative z-[998]  w-full  py-2">
-     <div className="mobile_navigation2"></div>
+    <div className="hidden navbar-main-group navbar-main-group4 relative z-[998] w-full py-2" ref={navbarRef}>
+      <div className="mobile_navigation2"></div>
       <div className="NavigationMenuRoot4 navbar-main relative">
         <div className="NavigationMenuList4 h-[42px] w-full">
           <div className="">
@@ -33,34 +49,30 @@ const MobileNavigation = () => {
             </Link>
           </div>
 
-          
-       {hamburger ?    <IoClose className="text-[20px] cursor-pointer" onClick={()=>{setHamburger(false)}}/> :    <FaBarsStaggered className="text-[20px] cursor-pointer" onClick={()=>{setHamburger(true), setNavbarStatus(true)}}/>}
-  
-    
+          {hamburger ? (
+            <IoClose className="text-[20px] cursor-pointer" onClick={() => { setHamburger(false); }} />
+          ) : (
+            <FaBarsStaggered className="text-[20px] cursor-pointer" onClick={() => { setHamburger(true); setNavbarStatus(true); }} />
+          )}
         </div>
-        {navbarStatus &&  hamburger  ? 
-    
-            <div className="overflow-hidden fixed top-12 left-0 w-full flex flex-col bg-black px-[1.2rem] py-2 ">
-                    <Link className="mobile_menu_link" onClick={()=>{setNavbarStatus(false),setHamburger(false)}} href='/case-studies'>Case Studies</Link>
-                    <Link className="mobile_menu_link"  onClick={()=>{setNavbarStatus(false),setHamburger(false)}} href='/about'>About Us</Link>                 
-                    <Link className="mobile_menu_link"  onClick={()=>{setNavbarStatus(false),setHamburger(false)}} href='/blog'>Blogs</Link>                 
-
-                    <MobileMenu setHamburger={setHamburger} setNavbarStatus={setNavbarStatus} pathname={pathname} title="Services" />
-                    <button
+        {navbarStatus && hamburger ? (
+          <div className="overflow-hidden fixed top-12 left-0 w-full flex flex-col bg-black px-[1.2rem] py-2 ">
+            <Link className="mobile_menu_link" onClick={() => { setNavbarStatus(false); setHamburger(false); }} href='/case-studies'>Case Studies</Link>
+            <Link className="mobile_menu_link" onClick={() => { setNavbarStatus(false); setHamburger(false); }} href='/about'>About Us</Link>
+            <Link className="mobile_menu_link" onClick={() => { setNavbarStatus(false); setHamburger(false); }} href='/blog'>Blogs</Link>
+            <MobileMenu setHamburger={setHamburger} setNavbarStatus={setNavbarStatus} pathname={pathname} title="Services" />
+            <button
               onClick={() => {
-                onHandleSubmit("/contact?action=message"),setNavbarStatus(false),setHamburger(false)
+                onHandleSubmit("/contact?action=message");
+                setNavbarStatus(false);
+                setHamburger(false);
               }}
               className="bg-cloudBtn text-center navbar_contactus_btn text-white my-2 px-2 py-2 rounded text-[14px]"
             >
               Contact Us
             </button>
-                   
-            </div>
-   
-      
-      :''}
-
-     
+          </div>
+        ) : ''}
       </div>
     </div>
   );

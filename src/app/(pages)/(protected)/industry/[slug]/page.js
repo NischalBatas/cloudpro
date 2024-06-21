cd5a159a5947a2f2b6c28bba4d4b34495e39de2d
Nischal Batas <nischal.batas88@gmail.com>
2024-06-21 14:56:23 +0545

    adding meta service tag

--- src/app/(pages)/(protected)/industry/[slug]/page.js
+++ src/app/(pages)/(protected)/industry/[slug]/page.js
@@ -1,5 +1,5 @@
 import IndustriesDetail from '@/components/Page/Homes/Content/IndustriesDetail/IndustriesDetail'
-import React from 'react'
+import { industriesDetails } from "@/db/home/industriesDetail/industriesDetail";import React from 'react'
 
 const page = (props) => {
   let slug = props.params.slug
@@ -11,3 +11,47 @@ const page = (props) => {
 }
 
 export default page
+
+export async function generateMetadata(props) {
+  let slug = props.params.slug;
+  const service = industriesDetails.find((item) => item.slug === slug);
+
+  if (!service) {
+    return {};
+  }
+
+  const imageUrl = service.introduction.image;
+
+  return {
+    title: service.introduction.title,
+    description: service.introduction.description,
+    // image: imageUrl,
+    openGraph: {
+      title: service.introduction.title,
+      description: service.introduction.description,
+      images: [
+        {
+          url: imageUrl, // Must be an absolute URL
+          width: 200,
+          height: 200,
+          alt: service.introduction.title,
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+      url: `${process.env.NEXT_PUBLIC_CLOUDPRO_URL}/service/${service.slug}`,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: service.introduction.title,
+      description: service.introduction.description,
+      creator: service.introduction.title,
+      images: [
+        {
+          url: imageUrl, // Must be an absolute URL
+          alt: service.introduction.title,
+        },
+      ],
+    },
+  };
+}

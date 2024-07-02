41663f208cc8771995ed59766ec4c1989c37648f
Nischal Batas <nischal.batas88@gmail.com>
2024-07-02 12:41:38 +0545

    adding segment

--- src/app/(pages)/layout.js
+++ src/app/(pages)/layout.js
@@ -4,7 +4,6 @@ import Footer from "@/components/Footer/Footer";
 import MobileNavigation from "@/components/Navbar/MobileMenu/MobileNavigation";
 import ProgressBar from "@/components/Animation/ProgressBar";
 import ScrollToTops from "@/components/ScrollToTop/ScrollToTop";
-import SegementRoute from "@/components/segment/SegementRoute";
 
 
 
@@ -15,7 +14,6 @@ const ProtectedLayout = ({ children }) => {
   return (
 
 <>
-<SegementRoute/>
  <ProgressBar/>
    {/* <Header /> */}
    <Navbar />

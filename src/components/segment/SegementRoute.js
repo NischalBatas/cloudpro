d544585005b535fc332130a913f425ab7384fc0c
Nischal Batas <nischal.batas88@gmail.com>
2024-07-02 11:54:09 +0545

    adding segment

---
+++ src/components/segment/SegementRoute.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useEffect } from 'react';
+import Router from 'next/navigation';
+
+const SegementRoute = () => {
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+          if (url) {
+            global.analytics.page('Loaded Another Website Page', {
+              page: url,
+            });
+          }
+        };
+    
+        Router.events.on('routeChangeComplete', handleRouteChange);
+    
+        return () => {
+          Router.events.off('routeChangeComplete', handleRouteChange);
+        };
+      }, []);
+  return (
+    <div></div>
+  )
+}
+
+export default SegementRoute

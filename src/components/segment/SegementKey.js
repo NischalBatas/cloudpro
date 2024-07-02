fe19986f1bf557657e5c54b846e277bd627baa06
Nischal Batas <nischal.batas88@gmail.com>
2024-07-02 11:30:07 +0545

    adding segment

--- src/components/segment/SegementKey.js
+++ src/components/segment/SegementKey.js
@@ -1,10 +1,28 @@
 import Script from 'next/script'
 import React from 'react'
+import * as snippet from '@segment/snippet'
 
 const SegementKey = () => {
+  function renderSnippet() {
+    const opts = {
+      apiKey: process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY
+   // note: the page option only covers SSR tracking.
+      // page: true,
+    }
+  
+    if (process.env.NODE_ENV === 'development') {
+      return snippet.max(opts)
+    }
+  
+    return snippet.min(opts)
+  }
   return (
    <>
-  <Script
+   <Script
+        id="segment-script"
+        dangerouslySetInnerHTML={{ __html: renderSnippet() }}
+      />
+  {/* <Script
 
       dangerouslySetInnerHTML={{
         __html: `
@@ -14,7 +32,7 @@ analytics.page();
 }}();
         `,
       }}
-    />
+    /> */}
    </>
   )
 }

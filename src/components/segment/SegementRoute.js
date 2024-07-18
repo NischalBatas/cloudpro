d908a94605543ff4169292530c8d5d00d94d1615
Nischal Batas <nischal.batas88@gmail.com>
2024-07-18 22:48:57 +0545

    added pathname

--- src/components/segment/SegementRoute.js
+++ src/components/segment/SegementRoute.js
@@ -4,35 +4,46 @@ import Router from 'next/router';
 
 const SegementRoute = ({ children }) => {
 
-
   useEffect(() => {
-    const handleRouteChange = (url) => {
+    const handleRouteChange = () => {
       const currentUrl = window.location.href;
       const currentPath = window.location.pathname;
       const currentSearch = window.location.search;
       const currentTitle = document.title;
       const referrer = document.referrer;
 
-      if (url) {
-        global.analytics.page({
-          path: currentPath,
-          referrer: referrer,
-          search: currentSearch,
-          title: currentTitle,
-          url: currentUrl,
-        });
-      }
+      global.analytics.page({
+        path: currentPath,
+        referrer: referrer,
+        search: currentSearch,
+        title: currentTitle,
+        url: currentUrl,
+      });
     };
 
     Router.events.on('routeChangeComplete', handleRouteChange);
 
-    // Track the initial page load
-    handleRouteChange(window.location.href);
-
     return () => {
       Router.events.off('routeChangeComplete', handleRouteChange);
     };
-  }, [Router.events]);
+  }, []);
+
+  useEffect(() => {
+    // Track the initial page load once when the component mounts
+    const currentUrl = window.location.href;
+    const currentPath = window.location.pathname;
+    const currentSearch = window.location.search;
+    const currentTitle = document.title;
+    const referrer = document.referrer;
+
+    global.analytics.page({
+      path: currentPath,
+      referrer: referrer,
+      search: currentSearch,
+      title: currentTitle,
+      url: currentUrl,
+    });
+  }, []);
 
   return (
     <div>

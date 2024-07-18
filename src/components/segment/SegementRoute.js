000140f4075157995313ff15a25b553843a3eae2
Nischal Batas <nischal.batas88@gmail.com>
2024-07-18 21:28:55 +0545

    added pathname

--- src/components/segment/SegementRoute.js
+++ src/components/segment/SegementRoute.js
@@ -1,35 +1,44 @@
 "use client"
-import React from 'react'
-import { useEffect } from 'react';
-
+import React, { useEffect } from 'react';
 import Router from 'next/router';
-import { usePathname } from 'next/navigation';
 
-const SegementRoute = ({children}) => {
-  const pathname=usePathname()
+const SegementRoute = ({ children }) => {
+
+
   useEffect(() => {
     const handleRouteChange = (url) => {
+      const currentUrl = window.location.href;
+      const currentPath = window.location.pathname;
+      const currentSearch = window.location.search;
+      const currentTitle = document.title;
+      const referrer = document.referrer;
+
       if (url) {
-        global.analytics.page('Loaded Another Website Page', {
-          page: `${url}/${pathname}`,
+        global.analytics.page({
+          path: currentPath,
+          referrer: referrer,
+          search: currentSearch,
+          title: currentTitle,
+          url: currentUrl,
         });
       }
     };
 
     Router.events.on('routeChangeComplete', handleRouteChange);
 
+    // Track the initial page load
+    handleRouteChange(window.location.href);
+
     return () => {
       Router.events.off('routeChangeComplete', handleRouteChange);
     };
-  }, []);
-    
+  }, [Router.events]);
+
   return (
-
     <div>
-      
-        {children}
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default SegementRoute
+export default SegementRoute;

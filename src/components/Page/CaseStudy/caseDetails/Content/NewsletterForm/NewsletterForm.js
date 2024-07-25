8d380110e8ad84f244d467d335ed4bea375b7665
Nischal Batas <nischal.batas88@gmail.com>
2024-07-25 12:07:53 +0545

    adding tags in newsletter

--- src/components/Page/CaseStudy/caseDetails/Content/NewsletterForm/NewsletterForm.js
+++ src/components/Page/CaseStudy/caseDetails/Content/NewsletterForm/NewsletterForm.js
@@ -22,7 +22,7 @@ const NewsletterForm = ({contentType,contentTitle,contentCategory}) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
-    const filterData=MailCheap.filter((item)=>item?.name?.toLowerCase() == contentType?.toLowerCase())
+    const filterData=MailCheap.filter((item)=>item.name.toLowerCase() == contentType.toLowerCase())
     console.log("url",contentType,filterData)
 
   // const handleSubmit = async (e) => {
@@ -150,13 +150,13 @@ const NewsletterForm = ({contentType,contentTitle,contentCategory}) => {
           />
         </Form.Control>
       </Form.Field>
-        {filterData &&   filterData.map((item,index)=>{
+        {filterData ?   filterData.map((item,index)=>{
             return(
               <div key={index}>
                 
-                <input type="hidden" name="tags" value={item.id}/></div>
+                <input type="hidden" name="tags" value={item?.id}/></div>
             )
-          })}
+          }):''}
         
 
   
